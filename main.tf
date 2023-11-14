6251d5f1f13e43c2ad2dd12b52e642c28701f1af
mahendran jayachandra <mahendran_res@yahoo.com>
2023-11-14 10:51:38 +0530

    create ssm params in console

---
+++ main.tf
@@ -0,0 +1,18 @@
+# create ssm parameters in AWS console
+
+resource "aws_ssm_parameter" "ssmparams" {
+  count = var.paramlists.length -1
+  name  = var.paramlists[count.index].name
+  type  = var.paramlists[count.index].type
+  value = var.paramlists[count.index].value
+}
+
+variable "paramlists" {
+    default = { 
+        catalogue = { name = "roboshop/dev/frontend/catalogue-dev", type = "String", value = "http://catalogue-dev.learnskill.fun:8080/"},
+        user = { name = "roboshop/dev/frontend/user-dev", type = "String", value = "http://user-dev.learnskill.fun:8080/" },
+        shipping = { name = "roboshop/dev/frontend/shipping-dev", type = "String", value = "http://shipping-dev.learnskill.fun:8080/" },
+        payment = { name = "roboshop/dev/frontend/payment-dev", type = "String", value = "http://payment-dev.learnskill.fun:8080/" }
+    }
+}
+
