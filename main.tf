056ca62891a26d9b93d48498c74b8ef7e0388775
mahendran jayachandra <mahendran_res@yahoo.com>
2023-11-14 15:53:16 +0530

    created ssm params in console

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@
 
 resource "aws_ssm_parameter" "ssmparams" {
   count = length(var.paramlists)
-  name  = var.paramlists[paramlists[count.index]].name
-  type  = var.paramlists[paramlists[count.index]].name
-  value = var.paramlists[paramlists[count.index]].name
+  name  = var.paramlists[count.index].name
+  type  = var.paramlists[count.index].type
+  value = var.paramlists[count.index].val
 }
 
 variable "paramlists" {
